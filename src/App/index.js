c0c520e18c470d01d57f51913017942ec2de6bf7
Cirovsky <cirovisqui@gmail.com>
2022-06-28 12:52:39 -0300

    Teclado agora é responsivo e adaptado src/App/index.js de Function para Class

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,37 @@
-import React, {useState} from "react";
+import React, { Component } from "react";
 import{
     SafeAreaView,
     View,
     Text,
-    TouchableOpacity,
 }
 from "react-native";
 import Board from "../components/Board";
 import KeyPad from "../components/KeyPad";
 import styles from "../styles";
 
-export default () => {
-    const [arrayHint, SetArrayHint] = useState(['TEST1'])
-    const [nextLine, SetNextLine] = useState(0)
+
+let word = ['TEST0','TEST1','TEST2', 'TEST3', 'TEST4','TEST5']
+
+export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = this.creatState()
+    }
+
+    creatState = () => {
+        return {
+            arrayHint: word,
+            nextLine: 0
+        }
+
+    }
+    
+    showLetter = (letter) => {
+        const word = this.state.arrayHint
+        word.splice(0,1,letter)
+    }
+
+    render(){
 
     return (
         <SafeAreaView style = {styles.main}>
@@ -22,12 +41,15 @@ export default () => {
                 </Text>
             </View>
             <View style = {styles.container}>
-                <Board word ={arrayHint[nextLine]} selLine={nextLine}/>
+                <Board word ={this.state.arrayHint[this.state.nextLine]} selLine={this.state.nextLine}/>
             </View>
             <View style = {[styles.container, {flex: 0.5}]}>
                 <KeyPad/>
             </View>
         </SafeAreaView>
     )
+      
+    }
+        
 }
 
--- src/components/KeyPad/Key.js
+++ src/components/KeyPad/Key.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {
     Text,
-    View
+    View,
+    TouchableHighlight
 } from "react-native";
 import styles from "../../styles";
 
 export default (props) => (
-    <View style = {[styles.key, 
+    <TouchableHighlight
+        onPress={()=> console.log(props.keyLetter)}
+    >
+        <View style = {[styles.key, 
     props.keyLetter ==='ENTER'? {width: 70}: '',
     props.keyLetter ==='<'? {width: 25, marginLeft: 5}: '',
     props.keyLetter ==='A'? {marginLeft: 10}: '']}>
@@ -15,4 +19,5 @@ export default (props) => (
             {props.keyLetter}
         </Text>
     </View>
+    </TouchableHighlight>
 )
